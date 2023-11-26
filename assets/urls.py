44b7a9402d258fcad610366088325c215007a8b4
Geronimo Ferrario <ferrario.dev@gmail.com>
2023-11-26 11:25:19 -0300

    feat(project setup): django, javaScript, html, css

---
+++ assets/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('add-assets/', views.add_asset, name="create"),
+]
---
+++ assets/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def add_asset(request):
+    return render(request, 'assets/add_asset.html')
